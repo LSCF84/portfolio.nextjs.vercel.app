4b69ac32172bcac1d0a58bb051411159ba343ed5
Luis Sergio <62592632+LSCF84@users.noreply.github.com>
2025-11-26 18:17:18 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,9 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Nikhil Rajput",
+  title: "LSCF",
   description:
-    "Nikhil Rajput is a proficient Software Engineer and Full Stack Developer from India, skilled in front-end and back-end development using modern tech stacks.",
+    "Tecnico IT Y Desarrollador Jr.",
   robots: {
     index: true,
     follow: true,
