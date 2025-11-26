594c72e4c7c344604468408b32e1a826ae1834db
Luis Sergio <62592632+LSCF84@users.noreply.github.com>
2025-11-26 21:46:18 +0100

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,22 +1,43 @@
 class Strings {
-  static telegram = "Telegram";
-  static telegramLink = "https://telegram.me/LSCF84";
-  static telegramUsername = "LSCF84";
+    static telegram = "Telegram";
+    static telegramLink = "https://telegram.me/LSCF84";
+    static telegramUsername = "LSCF84";
 
-  static github = "GitHub";
-  static githubLink = "https://github.com/LSCF84/";
-  static githubUsername = "LSCF84";
+    static github = "GitHub";
+    static githubLink = "https://github.com/LSCF84/";
+    static githubUsername = "LSCF84";
 
-  static linkedIn = "LinkedIn";
-  static linkedInLink = "https://www.linkedin.com/in/luis-sergio-camacho-fernandez-a49209141/";
-  static linkedInUsername = "LSCF84";
+    static linkedIn = "LinkedIn";
+    static linkedInLink = "https://www.linkedin.com/in/luis-sergio-camacho-fernandez-a49209141/";
+    static linkedInUsername = "LSCF84";
 
-  static email = "Email";
-  static primaryEmailLink = "l.sergio.camacho.fernandez@gmail.com";
-  static primaryEmail = "l.sergio.camacho.fernandez@gmail.com";
+    // ------------------------------------
+    // PROPIEDADES AÑADIDAS PARA SOLUCIONAR EL ERROR
+    // ------------------------------------
+    static instagram = "Instagram";
+    static instagramLink = ""; // Dejar vacío si no lo usas
+    static instagramUsername = "";
 
-  static fullName = "Luis Sergio Camacho Fernandez  ";
-  static shortName = "LSCF84";
+    static twitter = "Twitter";
+    static twitterLink = ""; // Dejar vacío si no lo usas
+    static twitterUsername = "";
+
+    // Elimina o añade estos dos si son necesarios para el código
+    static buyMeACoffee = "BuyMeACoffee";
+    static buyMeACoffeeLink = "";
+    static buyMeACoffeeUsername = "";
+
+    static koFi = "Ko-Fi";
+    static koFiLink = "";
+    static koFiUsername = "";
+    // ------------------------------------
+
+    static email = "Email";
+    static primaryEmailLink = "mailto:l.sergio.camacho.fernandez@gmail.com";
+    static primaryEmail = "l.sergio.camacho.fernandez@gmail.com";
+
+    static fullName = "Luis Sergio Camacho Fernandez";
+    static shortName = "LSCF84";
 }
 
 export default Strings;
